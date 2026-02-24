8c9e97f231b465c9db0c846e13ab3726161d3d58
Roger <rogerdesouza99@gmail.com>
2026-02-24 12:13:45 -0300

    Criando arquivos CSS globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Fragment } from 'react/jsx-runtime';
+import './styles/theme.css';
+import './styles/global.css';
+
+import { Heading } from './components/Heading';
 
 export function App() {
+  console.log('Oi');
+
   return (
-    <Fragment>
-      <h1>Olá Mundo!</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eligendi
         vitae sunt eum, ab sit ipsum, voluptas facilis quo unde provident?
         Facere cumque ullam nulla cupiditate vel, laboriosam officiis voluptas?
       </p>
-    </Fragment>
+    </>
   );
 }
