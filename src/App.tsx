b4f5ffdb587d74e32ce5863909e4d6374c1dacaf
Roger <rogerdesouza99@gmail.com>
2026-02-25 21:36:25 -0300

    Criando componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
+
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <section>Logo</section>
+        <Logo>Logo</Logo>
       </Container>
       <Container>
         <section>Menu</section>
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,1 +1,15 @@
-export { Container } from './Container';
+import styles from './styles.module.css';
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <section>{children}</section>
+      </div>
+    </div>
+  );
+}
